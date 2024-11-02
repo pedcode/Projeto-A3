c438347b0648509000f2b9a087a50b24011c89af
Pedro Arthur Canavezi <122173994+pedro-canavezi@users.noreply.github.com>
2024-11-02 01:04:02 -0300

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $dbHost = 'localhost';
-    $dbUsername = 'root';
-    $dbPassword = 'admin';
-    $dbName = 'cadastropedkids';
-
-    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-?>
